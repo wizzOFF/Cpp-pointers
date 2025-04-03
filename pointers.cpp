bec366f814c6bed0d538b25ee3e5a4125c3fa700
WizzOFF <maher_off@proton.me>
2025-04-03 14:01:33 +0200

    pointers.cpp: declare other pointers

--- pointers.cpp
+++ pointers.cpp
@@ -5,4 +5,19 @@ using namespace std;
 int main
 {
     int *pointeur;
+
+    double *pointeurA;
+    //Un pointeur qui peut contenir l'adresse d'un nombre à virgule
+    
+    unsigned int *pointeurB;
+    //Un pointeur qui peut contenir l'adresse d'un nombre entier positif
+    
+    string *pointeurC;
+    //Un pointeur qui peut contenir l'adresse d'une chaîne de caractères
+    
+    vector<int> *pointeurD;
+    //Un pointeur qui peut contenir l'adresse d'un tableau dynamique de nombres entiers
+    
+    int const *pointeurE;
+    //Un pointeur qui peut contenir l'adresse d'un nombre entier constant
 }
